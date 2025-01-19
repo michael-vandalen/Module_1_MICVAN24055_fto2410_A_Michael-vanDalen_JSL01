7d2b64f0585884c735a2a9ea252d1706dd676dfb
Michael van Dalen <michaelvdalen191@gmail.com>
2025-01-19 09:34:57 +0200

    Added comments to code

--- script.js
+++ script.js
@@ -5,9 +5,12 @@ function validateSyntax() {
 
   // TODO: Write your validation logic here
   // Check if input starts with 'pet_' and followed by alphanumeric characters
+
+  //Added a pattern for the input validation
   let pattern = /^pet_[0-9]{4}[A-Z]{1}[a-z]+$/;
 
   if (pattern.test(input)) {
+    //Checking if a valid syntax was entered
     result = "Valid Syntax";
   } else {
     result = "Invalid Syntax";
